24ce80d5ffa3753469cb68b5e7b2600fe9327e8b
Phavour-EBEN <ainooebenezer05@gmail.com>
2024-07-08 18:14:53 +0000

    writing to and reading of rile

--- 2.2 Native Modules/index.js
+++ 2.2 Native Modules/index.js
@@ -1,9 +1,9 @@
 const fs = require ('fs');
 
-fs.writeFile('message.txt', 'welcome Eben to node.js introduction', (err) =>{
-    if (err) throw err
-    console.log('The file has been saved!');
-});
+// fs.writeFile('message.txt', 'welcome Eben to node.js introduction', (err) =>{
+//     if (err) throw err
+//     console.log('The file has been saved!');
+// });
 
 
 fs.readFile('message.txt','utf8',(err,data)=>{
