0634055676e1a02b6954e07b5725cdbbf2989e8c
Phavour-EBEN <ainooebenezer05@gmail.com>
2024-07-09 17:56:49 +0000

    Installed some packages and generated some random names

---
+++ NPM/index.js
@@ -0,0 +1,10 @@
+// var randomGenName  = require("sillyname");
+// var RandomName = randomGenName();
+
+// console.log(`My name is ${RandomName}`);
+
+import superName from 'superheros';
+
+var Name = superName();
+
+console.log(`My superhero name is ${Name}`)
